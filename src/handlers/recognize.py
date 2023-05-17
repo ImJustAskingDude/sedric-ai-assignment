134e4447ad4ee2b8167862ec4bf1a2771fd636e9
ImJustAskingDude <bartosz.s.ziolo@gmail.com>
2023-05-17 08:31:01 +0200

    Now recognize handle executes test Parses event parameter Loads RecognizeRequest Creates S3MediaFileUri Creates SedricTranscription Creates AwsTranscriber Executes AwsTranscriber.transcribe Creates RecognizeResponse Retruns dumped response

--- src/handlers/recognize.py
+++ src/handlers/recognize.py
@@ -1,5 +1,49 @@
-def handle(event, context):
+import jsons
+
+from src.data_transfer.aws_event import AwsEvent
+from src.data_transfer.recognize_request import RecognizeRequest
+from src.data_transfer.recognize_response import RecognizeResponse
+from src.data_transfer.recognize_response_body import RecognizeResponseBody
+from src.domain.models.s3_media_file_uri import S3MediaFileUri
+from src.domain.models.sedric_transcription import SedricTranscription
+from src.services.aws_transcriber import AwsTranscriber
+
+
+def handle(event: dict, context):
+    print(event)
+    aws_event = jsons.load(json_obj=event, cls=AwsEvent)
+    aws_event_body = aws_event.body
+
+    request = jsons.loads(aws_event_body, RecognizeRequest)
+    print(request)
+
+    bucket_name = "sedric-transcription-audio"
+
+    s3_media_file_uri = S3MediaFileUri(
+        bucket_name=bucket_name, file_name="still-listening-gentleman.wav"
+    )
+
+    transcription = SedricTranscription(
+        job_name="sample_transcription_job", s3_media_file_uri=s3_media_file_uri
+    )
+
+    transcription_service = AwsTranscriber()
+    transcription_response = transcription_service.transcribe(
+        transcription=transcription
+    )
+
+    print(f"{transcription_response=}")
+
+    response = RecognizeResponse(
+        body=RecognizeResponseBody(
+            request_id="generated_request_id",
+            message="Your request was accepted successfully",
+        )
+    )
+
     return {
-        'code': 200,
-        'message': 'Yeah'
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": {"Content-Type": "application/json"},
+        "body": jsons.dumps(response),
     }
