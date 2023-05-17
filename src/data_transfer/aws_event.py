ec9830e52a5c4466ff51cf07cbe9d2bb29198d73
ImJustAskingDude <bartosz.s.ziolo@gmail.com>
2023-05-18 00:30:25 +0200

    Removed AwsEvent extranous irrelevant classes

--- src/data_transfer/aws_event.py
+++ src/data_transfer/aws_event.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
-from types import NoneType
+from typing import Optional
 
 
 class HttpMethod(Enum):
@@ -9,51 +9,9 @@ class HttpMethod(Enum):
 
 
 @dataclass(frozen=True)
-class AwsEventRequestContextIdentity:
-    cognitoIdentityPoolId: NoneType
-    cognitoIdentityId: NoneType
-    apiKey: str
-    principalOrgId: NoneType
-    cognitoAuthenticationType: NoneType
-    userArn: str
-    apiKeyId: str
-    userAgent: str
-    accountId: str
-    caller: str
-    sourceIp: str
-    accessKey: str
-    cognitoAuthenticationProvider: NoneType
-    user: str
-
-
-@dataclass(frozen=True)
-class AwsEventRequestContext:
-    resourceId: str
-    resourcePath: HttpMethod
-    extendedRequestId: str
-    requestTime: str
-    path: str
-    accountId: str
-    protocol: str
-    stage: str
-    domainPrefix: str
-    requestTimeEpoch: int
-    requestId: str
-    identity: AwsEventRequestContextIdentity
-    domainName: str
-    apiId: str
-
-
-@dataclass(frozen=True)
 class AwsEvent:
     resource: str
     path: str
     httpMethod: HttpMethod
-    headers: NoneType
-    multiValueHeaders: NoneType
-    queryStringParameters: NoneType
-    multiValueQueryStringParameters: NoneType
-    pathParameters: NoneType
-    stageVariables: NoneType
-    body: str
+    body: Optional[str]
     isBase64Encoded: bool
