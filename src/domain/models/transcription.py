def63e35e6389af85cff7fe19afa1e358892f786
ImJustAskingDude <bartosz.s.ziolo@gmail.com>
2023-05-18 21:22:42 +0200

    Now Transcription implements getter methods for all fields

--- src/domain/models/transcription.py
+++ src/domain/models/transcription.py
@@ -19,6 +19,18 @@ class Transcription:
         self._media_format = media_format
         self._media_file_uri = media_file_uri
 
+    def get_job_name(self) -> str:
+        return self._job_name
+    
+    def get_language_code(self) -> LanguageCode:
+        return self._language_code
+    
+    def get_media_format(self) -> MediaFormat:
+        return self._media_format
+    
+    def get_media_file_uri(self) -> MediaFileUri:
+        return self._media_file_uri
+
     @staticmethod
     def allowed_language_codes() -> List[str]:
         return get_enum_values(LanguageCode)
