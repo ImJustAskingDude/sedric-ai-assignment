562a843a2c641374d977b1b12046c7ae2657b306
ImJustAskingDude <bartosz.s.ziolo@gmail.com>
2023-05-19 00:09:36 +0200

    Removed audio_upload.py script

--- src/handlers/aws_lambda/audio_upload.py
+++
@@ -1,9 +0,0 @@
-def handle(event: dict, context):
-    print(event)
-
-    return {
-        "isBase64Encoded": False,
-        "statusCode": 200,
-        "headers": {"Content-Type": "application/json"},
-        "body": {},
-    }
