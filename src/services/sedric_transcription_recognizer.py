446bb7c35da8d5e978183093f411b08874c91ee3
ImJustAskingDude <bartosz.s.ziolo@gmail.com>
2023-05-18 21:51:37 +0200

    Now SedricTranscriptionRecognizer implements TranscriptionRecognizer

--- src/services/sedric_transcription_recognizer.py
+++ src/services/sedric_transcription_recognizer.py
@@ -1,3 +1,11 @@
-class SedricTranscriptionRecognizer:
+from typing import List
+
+from services.interfaces.recognizer import TranscriptionRecognizer
+
+
+class SedricTranscriptionRecognizer(TranscriptionRecognizer):
     def __init__(self) -> None:
+        pass
+
+    def recognize(self, transcription_text: str, sentences: List[str]):
         pass
