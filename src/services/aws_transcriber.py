38ba0c78ea7ceea0d6fb84694041ce3fcfaaf6e5
ImJustAskingDude <bartosz.s.ziolo@gmail.com>
2023-05-18 21:11:16 +0200

    Now AwsTranscriber only accepts AwsTranscriptionRequest

--- src/services/aws_transcriber.py
+++ src/services/aws_transcriber.py
@@ -1,4 +1,5 @@
 import boto3
+from data_transfer.aws_transcription_request import AwsTranscriptionRequest
 from domain.models.transcription import Transcription
 
 from services.interfaces.transcriber import Transcriber
@@ -8,14 +9,14 @@ class AwsTranscriber(Transcriber):
     def __init__(self) -> None:
         self._client = boto3.client("transcribe")
 
-    def transcribe(self, transcription: Transcription, output_bucket_name: str, output_key: str):
+    def transcribe(self, transcription_request: AwsTranscriptionRequest):
         response = self._client.start_transcription_job(
-            TranscriptionJobName=transcription._job_name,
-            LanguageCode=transcription._language_code.value,
-            MediaFormat=transcription._media_format.value,
-            Media={"MediaFileUri": transcription._media_file_uri.get_file_name()},
-            OutputBucketName=output_bucket_name,
-            OutputKey=output_key,
+            TranscriptionJobName=transcription_request.job_name,
+            LanguageCode=transcription_request.language_code,
+            MediaFormat=transcription_request.media_format,
+            Media={"MediaFileUri": transcription_request.media_file_uri},
+            OutputBucketName=transcription_request.output_bucket_name,
+            OutputKey=transcription_request.output_key,
         )
 
         return response
