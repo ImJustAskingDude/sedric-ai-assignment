4e447aa325160d8c59368d9a64f306030ed89267
ImJustAskingDude <bartosz.s.ziolo@gmail.com>
2023-05-18 21:13:08 +0200

    Created AwsEventSns data transfer class

---
+++ src/data_transfer/aws_event_sns.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass(frozen=True)
+class AwsEventSnsRecordSnsField:
+    Message: str
+
+
+@dataclass(frozen=True)
+class AwsEventSnsRecord:
+    Sns: AwsEventSnsRecordSnsField
+
+
+@dataclass(frozen=True)
+class AwsEventSns:
+    Records: List[AwsEventSnsRecordSnsField]
