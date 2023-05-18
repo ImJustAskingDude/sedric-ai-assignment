243a32d043623c2d092fc6efe738979e41067215
ImJustAskingDude <bartosz.s.ziolo@gmail.com>
2023-05-18 21:23:05 +0200

    Now TranscriptionRequest implements from_transcription classmethod

--- src/data_transfer/transcription_request.py
+++ src/data_transfer/transcription_request.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from domain.models.transcription import Transcription
+
 
 @dataclass(frozen=True)
 class TranscriptionRequest:
@@ -7,3 +9,12 @@ class TranscriptionRequest:
     language_code: str
     media_format: str
     media_file_uri: str
+
+    @classmethod
+    def from_transcription(cls: 'TranscriptionRequest', transcription: Transcription) -> 'TranscriptionRequest':
+        return TranscriptionRequest(
+            job_name=transcription.get_job_name(),
+            language_code=transcription.get_language_code().value,
+            media_format=transcription.get_media_format().value,
+            media_file_uri=transcription.get_media_file_uri().get_file_name()
+        )
