1acef547a3bc301c9db97694fd3486ccfa0bb9b7
ImJustAskingDude <bartosz.s.ziolo@gmail.com>
2023-05-18 21:38:14 +0200

    Now SedricAwsTranscriber transcribe creates AwsTranscriptionRequest

--- src/services/sedric_aws_transcriber.py
+++ src/services/sedric_aws_transcriber.py
@@ -1,3 +1,4 @@
+from data_transfer.aws_transcription_request import AwsTranscriptionRequest
 from data_transfer.transcription_request import TranscriptionRequest
 from services.aws_transcriber import AwsTranscriber
 from services.interfaces.transcriber import Transcriber
@@ -8,8 +9,15 @@ class SedricAwsTranscriber(Transcriber):
         self._aws_transcriber = aws_transcriber
 
     def transcribe(self, transcription_request: TranscriptionRequest):
-        return self._aws_transcriber.transcribe(
-            transcription_request=transcription_request,
+        aws_transcription_request = AwsTranscriptionRequest(
+            job_name=transcription_request.job_name,
+            language_code=transcription_request.language_code,
+            media_format=transcription_request.media_format,
+            media_file_uri=transcription_request.media_file_uri,
             output_bucket_name="sedric-transcription-results",
             output_key=f"{transcription_request.job_name}.json",
+        )
+
+        return self._aws_transcriber.transcribe(
+            transcription_request=aws_transcription_request,
         )
