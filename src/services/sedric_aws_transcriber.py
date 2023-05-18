f14176aea8ec036085fc15d69b356ace230d9d97
ImJustAskingDude <bartosz.s.ziolo@gmail.com>
2023-05-18 21:25:10 +0200

    Now SedricAwsTranscriber is a Transcriber

--- src/services/sedric_aws_transcriber.py
+++ src/services/sedric_aws_transcriber.py
@@ -1,12 +1,13 @@
-from data_transfer.aws_transcription_request import AwsTranscriptionRequest
+from data_transfer.transcription_request import TranscriptionRequest
 from services.aws_transcriber import AwsTranscriber
+from services.interfaces.transcriber import Transcriber
 
 
-class SedricAwsTranscriber:
+class SedricAwsTranscriber(Transcriber):
     def __init__(self, aws_transcriber: AwsTranscriber) -> None:
         self._aws_transcriber = aws_transcriber
 
-    def transcribe(self, transcription_request: AwsTranscriptionRequest):
+    def transcribe(self, transcription_request: TranscriptionRequest):
         return self._aws_transcriber.transcribe(
             transcription_request=transcription_request,
             output_bucket_name="sedric-transcription-results",
