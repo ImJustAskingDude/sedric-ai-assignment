10fc47cceafdb12e2b95abdf4a1c6257c8dd5dd2
ImJustAskingDude <bartosz.s.ziolo@gmail.com>
2023-05-18 20:55:30 +0200

    Now Transcriber transcribe also accepts output_bucket_name and output_key parameters

--- src/services/aws_transcriber.py
+++ src/services/aws_transcriber.py
@@ -8,12 +8,14 @@ class AwsTranscriber(Transcriber):
     def __init__(self) -> None:
         self._client = boto3.client("transcribe")
 
-    def transcribe(self, transcription: Transcription):
+    def transcribe(self, transcription: Transcription, output_bucket_name: str, output_key: str):
         response = self._client.start_transcription_job(
             TranscriptionJobName=transcription._job_name,
             LanguageCode=transcription._language_code.value,
             MediaFormat=transcription._media_format.value,
             Media={"MediaFileUri": transcription._media_file_uri.get_file_name()},
+            OutputBucketName=output_bucket_name,
+            OutputKey=output_key,
         )
 
         return response
--- src/services/interfaces/transcriber.py
+++ src/services/interfaces/transcriber.py
@@ -5,5 +5,5 @@ from domain.models.transcription import Transcription
 
 class Transcriber(ABC):
     @abstractmethod
-    def transcribe(self, transcription: Transcription):
+    def transcribe(self, transcription: Transcription, output_bucket_name: str, output_key: str):
         ...
